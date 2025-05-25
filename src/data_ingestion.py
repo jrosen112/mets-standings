6597016e24ed879185189d54a62be254a3adefd5
Jared Rosen <jaredrosen@Mac.lan>
2025-05-25 15:13:15 -0400

    helpful URLs

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -1,24 +1,16 @@
 import requests
 import pprint
-from Division import Division
 
-MLB_BASE_URL = "https://statsapi.mlb.com/api/v1/"
-BASEBALL_URL_PARAM = "divisions?sportId=1"
+NL_EAST_STANDINGS_URL = "http://statsapi.mlb.com/api/v1/standings/?leagueId=104"
+METS_TEAM_URL = "http://statsapi.mlb.com/api/v1/teams/121"
+CITI_FIELD_URL = "http://statsapi.mlb.com/api/v1/venues/3289"
 
 def make_request(req_url: str) -> dict:
     response = requests.get(req_url)
     return response.json()
 
-def construct_url(division: Division = None) -> str:
-    if not division:
-        return MLB_BASE_URL + BASEBALL_URL_PARAM
-    # TODO: return link for data for that div's leaderboard
-    # return MLB_BASE_URL + "divisions?" + BASEBALL_URL_PARAM + "&" + division.value.get("link")
-
 def main():
-    # url = construct_url(Division.NLE)
-    url = construct_url()
-    response = make_request(url)
+    response = make_request(CITI_FIELD_URL)
     pprint.pprint(response)
 
 if __name__ == "__main__":
